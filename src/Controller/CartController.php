1fbe68c95b5e1048a5e843fd932e89e47ea83b73
umm-habibah <ummhabibah-ouattara@outlook.com>
2022-05-23 17:36:51 +0200

    Ajout d'un article dans le panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classes\Cart;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart", name="cart")
+     */
+    public function index(Cart $cart): Response
+    {
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart->getAll()
+        ]);
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart_add")
+     */
+    public function add(Cart $cart, $id): Response
+    {
+        $cart->add($id);
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="cart_delete")
+     */
+    public function delete(Cart $cart, $id): Response
+    {
+        $cart->delete($id);
+        return $this->redirectToRoute('cart');
+    }
+}
